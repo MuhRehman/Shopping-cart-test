9b80cdb667a19e6d800254cddf9ae5e3b66faf37
Rehman <abdulrehman1@aol.com>
2021-05-07 15:02:38 +0500

    Intial commmit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="py-5 bg-dark">
+            <div className="container">
+                <p className="m-0 text-center text-white">Copyright © <a href="https://rehman-portfolio.netlify.app/">Abdul Rehman</a></p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
